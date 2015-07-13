7f06cd0d5e6de8c334e3622fd73ebd4ad4e5a685
askdziner <askdziner@gmail.com>
2015-07-13 09:43:20 +0500

    changes in contact form

--- mailer.php
+++ mailer.php
@@ -1,11 +1,27 @@
 <?php
+
+if(isset($_POST['name'])&&isset($_POST['email'])&&$_POST['name']!=""&&$_POST['email']!="")
+{
 	$name= $_POST['name']; 
     $email= $_POST['email']; 
     $message= $_POST['message']; 
-          
-        $from="From: $name<$email>\r\nReturn-path: $email"; 
-        $subject="Message sent using your contact form"; 
-        if(@mail("info@agilebudi.co.uk", $subject, $message, $from))
+    $to = "info@agilebudi.co.uk";
+	
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+	
+	// More headers
+	$headers .= 'From: <'.$email.'>' . "\r\n";
+	//$headers .= 'Cc: myboss@example.com' . "\r\n";
+	 $subject="Message sent using your contact form"; 
+
+	   $body = "Name: ".$name.'<br/>';
+	   $body .= "Email: ".$email.'<br/>';
+	   $body .= "Message: <br/>".nl2br($message).'<br/>';
+	      
+       
+       
+        if(@mail($to,$subject,$body,$headers))
 		{
 			echo "Email Sent";	
 		}
@@ -13,6 +29,8 @@
 			echo "Error";
 		}
         
+}
+	
 		
 		 
      
